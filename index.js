a6b154ec27f7169e5a2b9875475e61be01bb0c05
Tyll Weiß <info@craft-interactive.de>
2016-08-20 14:35:05 +0200

    CLEANUP: Code style consistency

--- index.js
+++ index.js
@@ -15,35 +15,35 @@ module.exports = {
     browser : true
   },
   rules: {
-	  //
-	  // Disable rules which aren't that important for us as a team.
-	  //
-      'react/jsx-boolean-value': 0,
-      'react/jsx-filename-extension': 0,
+    //
+    // Disable rules which aren't that important for us as a team.
+    //
+    'react/jsx-boolean-value': 0,
+    'react/jsx-filename-extension': 0,
 
-	  //
-	  // Switch to a 4 space indentation.
-	  //
-      'react/jsx-indent-props': [2, 4],
-      'react/jsx-indent': [2, 4],
-      'indent': [2, 4, {SwitchCase: 1}],
+    //
+    // Switch to a 4 space indentation.
+    //
+    'react/jsx-indent-props': [2, 4],
+    'react/jsx-indent': [2, 4],
+    'indent': [2, 4, {SwitchCase: 1}],
 
-      //
-      // Enforce a basic level of accessibility.
-      //
-      'jsx-a11y/aria-props': 2,
-      'jsx-a11y/aria-proptypes': 2,
-      'jsx-a11y/aria-role': 2,
-      'jsx-a11y/aria-unsupported-elements': 2,
-      'jsx-a11y/href-no-hash': [ 2, [ 'Link', 'Anchor' ] ],
-      'jsx-a11y/img-has-alt': [ 2, 'Image' ],
-      'jsx-a11y/img-redundant-alt': 2,
-      'jsx-a11y/label-has-for': [ 2, 'Label' ],
-      'jsx-a11y/mouse-events-have-key-events': 2,
-      'jsx-a11y/no-access-key': 2,
-      'jsx-a11y/onclick-has-role': 1,
-      'jsx-a11y/role-has-required-aria-props': 2,
-      'jsx-a11y/role-supports-aria-props': 2,
-      'jsx-a11y/tabindex-no-positive': 1
+    //
+    // Enforce a basic level of accessibility.
+    //
+    'jsx-a11y/aria-props': 2,
+    'jsx-a11y/aria-proptypes': 2,
+    'jsx-a11y/aria-role': 2,
+    'jsx-a11y/aria-unsupported-elements': 2,
+    'jsx-a11y/href-no-hash': [ 2, [ 'Link', 'Anchor' ] ],
+    'jsx-a11y/img-has-alt': [ 2, 'Image' ],
+    'jsx-a11y/img-redundant-alt': 2,
+    'jsx-a11y/label-has-for': [ 2, 'Label' ],
+    'jsx-a11y/mouse-events-have-key-events': 2,
+    'jsx-a11y/no-access-key': 2,
+    'jsx-a11y/onclick-has-role': 1,
+    'jsx-a11y/role-has-required-aria-props': 2,
+    'jsx-a11y/role-supports-aria-props': 2,
+    'jsx-a11y/tabindex-no-positive': 1
   }
 }
