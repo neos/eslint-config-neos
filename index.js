9f6272c5935bc59d6fe3ee12beffde252f3bbd88
Markus Günther <info@unikka.de>
2022-11-16 13:22:13 +0100

    TASK: Upgrade to @babel/eslint-parser
    As babel-eslint is deprecated we need to use now @babel/eslint-parser

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  parser: 'babel-eslint',
+  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaFeatures: {
       legacyDecorators: true
--- index.spec.js
+++ index.spec.js
@@ -1,23 +1,23 @@
-const isPlainObj = require('is-plain-obj');
-const eslint = require('eslint');
-const tempWrite = require('temp-write');
-const conf = require('./index.js');
+const isPlainObj = require("is-plain-obj");
+const eslint = require("eslint");
+const tempWrite = require("temp-write");
+const conf = require("./index.js");
 
 const fixture = `'use strict';\nconst x = true;\n\nif (x) {\n    console.log();\n}\n`;
 
 function runEslint(str, conf) {
     const linter = new eslint.CLIEngine({
         useEslintrc: false,
-        configFile: tempWrite.sync(JSON.stringify(conf))
+        configFile: tempWrite.sync(JSON.stringify(conf)),
     });
 
     return linter.executeOnText(str).results[0].messages;
 }
 
-describe('eslint config', () => {
-    it('is a valid eslint config object', () => {
+describe("eslint config", () => {
+    it("is a valid eslint config object", () => {
         expect(isPlainObj(conf)).toBe(true);
         expect(isPlainObj(conf.rules)).toBe(true);
         expect(runEslint(fixture, conf).length).toBe(0);
-    })
+    });
 });
